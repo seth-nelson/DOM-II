0194f7f5cf183cb6af1a6be43f9026812eb3bda9
Seth Nelson <seth.nelson@outlook.com>
2020-01-21 18:14:13 -0800

    finished DOM II project. Added a bunch of different event listeners to different areas of the page.

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-// fun-bus img resie
+//fun bus image resize
 
 const funBusImage = document.querySelector('.container .intro img');
     funBusImage.addEventListener('mouseenter', (e) => {
@@ -11,13 +11,18 @@ funBusImage.addEventListener('mouseleave', (e) => {
 });
 
 
+//----------------------------------------------------------------------------------
+//nav color change
+
+
 const navColorChange = document.querySelector('.main-navigation');
     navColorChange.addEventListener('mouseenter', (e) => {
         event.target.style.backgroundColor = '#0ec0b1';
     });
 
 
-
+//---------------------------------------------------------------------------------
+//event handlers
 
 
 const eventHandlerOne = event => {
@@ -32,8 +37,14 @@ const eventHandlerTwo = event => {
     event.target.style.transition = 'all 2.0s';
 };
 
+const eventHandlerThree = event => {
+    event.target.style.transition = 'all 2.5s';
+    event.target.style.opacity = '0';
+};
 
 
+//------------------------------------------------------------------------------------
+//header color changes
 
 
 const header2ColorChange = document.querySelectorAll('.container h2');
@@ -46,7 +57,43 @@ const header4ColorChange = document.querySelectorAll('.container h4');
     header4ColorChange.forEach(header4 => {
         header4.addEventListener('click', eventHandlerTwo);
     })
-// headerColorChange.addEventListener('click', (e) => {
-//     event.target.style.color = 'yellow';
-//     event.target.style.transition = 'all 2.0s';
-// });
+
+
+//-----------------------------------------------------------------------------------
+    //adventure image dissapear
+
+    const adventureImage = document.querySelector('.img-content img');
+    adventureImage.addEventListener('mouseenter', (e) => {
+        event.target.style.transform = 'scale(0)';
+        event.target.style.transition = 'all 2.0s';
+    });
+
+adventureImage.addEventListener('mouseleave', (e) => {
+    event.target.style.transform = 'scale(1)'
+});
+
+
+    //-----------------------------------------------------------------------------------
+    //fun image dissapear
+
+    const funImage = document.querySelector('.img-fluid');
+    funImage.addEventListener('mouseenter', (e) => {
+        event.target.style.transform = 'scale(4)';
+        event.target.style.transition = 'all 1.25s';
+        event.target.style.opacity = '0';
+    });
+
+funImage.addEventListener('mouseleave', (e) => {
+    event.target.style.transform = 'scale(1)'
+    event.target.style.opacity = '100';
+});
+
+
+//---------------------------------------------------------------------------------------
+//dissapearing p tags
+
+
+const fadingParagraphs = document.querySelectorAll('.container p');
+    fadingParagraphs.forEach(ptags => {
+        ptags.addEventListener('click', eventHandlerThree);
+    })
